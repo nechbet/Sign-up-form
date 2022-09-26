bd4772f46082b1e89a12b9c3cfe196c4cd1e8a5c
Kristina Gedminaite <105384146+nechbet@users.noreply.github.com>
2022-09-26 21:43:43 +0300

    feat: :sparkles: js for password validation

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Time to script!
+function matchPasswords() {
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('confirm-password').value;
+  if(password === confirmPassword) {
+    alert('Account created successfully')
+  } else {
+    alert('Passwords do not match')
+  }
+};
